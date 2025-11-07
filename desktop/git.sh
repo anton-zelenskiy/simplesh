f63763b32049726d7722be5673f427c138312cc6
anton-zelenskiy <antonfewwt@gmail.com>
2025-11-07 16:38:48 +0700

    fix

--- desktop/git.sh
+++ desktop/git.sh
@@ -5,8 +5,4 @@ git.sh(){
     sudo apt install -y git;
     git config --global user.email "antonfewwt@gmail.com";
     git config --global user.name "anton-zelenskiy";
-    
-    mkdir ~/.ssh;
-    touch ~/.ssh/authorized_keys;
-    ssh-agent -s;
 }
--- desktop/sshkey.sh
+++ desktop/sshkey.sh
@@ -2,6 +2,10 @@
 
 sshkey.sh(){
   clear;
+  mkdir ~/.ssh;
+  touch ~/.ssh/authorized_keys;
+  ssh-agent -s;
+
   ssh-keygen -o -t ed25519 -f ~/.ssh/id_ed25519_github;
   cat ~/.ssh/id_ed25519_github.pub;
   ssh-add ~/.ssh/id_ed25519_github;
